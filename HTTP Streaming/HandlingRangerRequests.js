d5b1339bd7997bbe4b4cf41d5ceddee4917684b7
mohamedmfouad <mohamed.m.fouad.mahmoud@gmail.com>
2019-04-26 17:47:30 +0200

    range

---
+++ HTTP Streaming/HandlingRangerRequests.js
@@ -0,0 +1,34 @@
+const { createServer }  = require('http');
+const { stat, createReadStream } = require('fs');
+const { promisify } = require('util')
+ 
+const fileName = './powder-day.mp4';
+const fileInfo = promisify(stat);
+
+createServer(async(req, res)=>{
+    const { size } = await fileInfo(fileName);
+    const range = req.headers.range;
+    console.log('range 1 : ', range  );
+    if(range){
+      let [start, end]  = range.replace(/bytes=/,'').split('-');
+      start = parseInt(start,10);
+      end = end ? parseInt(end,10) :size -1 ;
+      res.writeHead(206,{
+          'Content-Range' : `bytes ${start} - ${ end }/ ${size}`,
+          'Accept-Ranges' : 'bytes',
+          'Content-Length' : (end - start) +1,
+          'Content-Type':'video/mp4'
+      })
+      console.log('range 2: ', range  );
+
+      createReadStream(fileName,{start, end}).pipe(res);
+    }else{
+        res.writeHead(200, {
+            'Content-length':size,
+            'Content-Type':'video/mp4'});
+        createReadStream(fileName).pipe(res);
+    }    
+
+}).listen(3000,()=>{
+    console.log('server running  - 30000');
+})
