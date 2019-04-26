33497d7452e02192754bff183c3012eb53da2ddb
mohamedmfouad <mohamed.m.fouad.mahmoud@gmail.com>
2019-04-26 17:48:40 +0200

    createReadStream

--- HTTP Streaming/StreamingToTheBrowser.js
+++ HTTP Streaming/StreamingToTheBrowser.js
@@ -1,11 +1,13 @@
 const { createServer }  = require('http');
-
-const { createReadStream } = require('fs');
-
+const { stat, createReadStream } = require('fs');
+const { promisify } = require('util')
+ 
 const fileName = './powder-day.mp4';
+const fileInfo = promisify(stat);
 
-createServer((req, res)=>{
-    res.writeHead(200, {'Content-Type':'video/mp4'});
+createServer(async(req, res)=>{
+    const { size } = await fileInfo(fileName);
+    res.writeHead(200, {'Content-length':size,'Content-Type':'video/mp4'});
     createReadStream(fileName).pipe(res);
 
 }).listen(3000,()=>{
